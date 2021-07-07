8f4d842f35e29b34c7ac5d355fde0c2ac87972c8
Sang T. Truong <sttruong@stanford.edu>
2021-07-07 15:17:36 -0400

    Adapt new configureation

--- run/main.py
+++ run/main.py
@@ -6,75 +6,78 @@ sys.path.insert(0, parentdir)
 from hybcast.models.DeployedESTransformer import DeployedESTransformer
 from features.Comparison import *
 from data.make_dataset import *
-from hybcast.models.utils_configs import get_config
+from config import cfg
 import argparse
 
 
-def main(args):
-  config = get_config(args.dataset)
-  if config['data_parameters']['frequency'] == 'Y': config['data_parameters']['frequency'] = None
+def main(cfg):
+  # Checking data directory
+  input_directory = cfg['dataset']['input_directory']
+  if input_directory == None: 
+    data_dir = parentdir.replace("\hybcast", "")
 
-  #Setting needed parameters
-  os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
+    input_directory = data_dir + "\\hybcast\\data\\external"
+    if os.path.isdir(input_directory) == False:
+      print("Creating data directory " + input_directory)
+      os.makedirs(input_directory)
+  else: 
+    if os.path.isdir(input_directory) == False:
+      print("Creating data directory " + input_directory)
+      os.makdirs(input_directory)
 
-  if args.num_obs: num_obs = args.num_obs
-  else: num_obs = 100000
-  # else: num_obs = 4000
+  # Checking cuda availability
+  if torch.cuda.is_available() == False: 
+    print("CUDA is not available, using cpu instead")
+    cfg['device'] = 'cpu'
+  else: print("Using CUDA")
 
-  if args.input_directory == None: args.input_directory = os.getcwd() + "\\data\\external"
-  else: assert os.path.isfile(args.input_directory), "No available path. Please try again"
 
-  if args.use_cpu == 1: config['device'] = 'cpu'
-  else: assert torch.cuda.is_available(), 'No cuda devices detected. You can try using CPU instead.'
-  
-  print('Reading data')
-  X_train_df, y_train_df, X_test_df, y_test_df = prepare_m4_data(dataset_name=args.dataset,
-                                                                 directory=args.input_directory,
-                                                                 num_obs=num_obs) 
+  print('Preparing M4 data - ' + cfg['dataset']['name'])
+  X_train_df, y_train_df, X_test_df, y_test_df = prepare_m4_data(dataset_name=cfg['dataset']['name'],
+                                                                 directory=input_directory,
+                                                                 num_obs=cfg["dataset"]["num_obs"] ) 
 
   print("Successfully get the M4 data.\nBegin ESTransformer")                                     
   #Instantiate the model
-  model = DeployedESTransformer(max_epochs = 500,
-                                batch_size = config['train_parameters']['batch_size'],
-                                freq_of_test = config['train_parameters']['freq_of_test'],
-                                learning_rate = float(config['train_parameters']['learning_rate']),
+  model = DeployedESTransformer(# Device and dataset
+                                device= cfg['device'],
+                                root_dir= input_directory,
+                                dataset_name = cfg['dataset']['name'],
 
-                                per_series_lr_multip = config['train_parameters']['per_series_lr_multip'],
-                                lr_scheduler_step_size = config['train_parameters']['lr_scheduler_step_size'],
-                                lr_decay = config['train_parameters']['lr_decay'],
+                                # Train parameters
+                                max_epochs = 500,
+                                batch_size = cfg['train_parameters']['batch_size'],
+                                freq_of_test = cfg['train_parameters']['freq_of_test'],
+                                learning_rate = float(cfg['train_parameters']['learning_rate']),
+                                per_series_lr_multip = cfg['train_parameters']['per_series_lr_multip'],
+                                lr_scheduler_step_size = cfg['train_parameters']['lr_scheduler_step_size'],
+                                lr_decay = cfg['train_parameters']['lr_decay'],
+                                level_variability_penalty = cfg['train_parameters']['level_variability_penalty'],
+                                testing_percentile = cfg['train_parameters']['testing_percentile'],
+                                training_percentile = cfg['train_parameters']['training_percentile'],
+                                ensemble = cfg['train_parameters']['ensemble'],
 
+                                # ES parameters 
+                                seasonality = cfg['ES_parameters']['seasonality'],
+                                random_seed = cfg['ES_parameters']['random_seed'],
+                                input_size = cfg['ES_parameters']['input_size'],
+                                d_input = cfg['ES_parameters']['d_input'], # input_size + 6 (which is exo features)
+                                output_size = cfg['ES_parameters']['output_size'], # must be the same as d_output
+                                d_output = cfg['ES_parameters']['d_output'], # input_size + output_size >= 13
+                                
+                                # Transformer parameter
                                 transformer_weight_decay = 0.0,
-
-                                level_variability_penalty = config['train_parameters']['level_variability_penalty'],
-                                
-                                testing_percentile = config['train_parameters']['testing_percentile'],
-                                training_percentile = config['train_parameters']['training_percentile'],
-                                
-                                ensemble = config['train_parameters']['ensemble'], 
-                                seasonality = config['data_parameters']['seasonality'],
-                                
-                                random_seed = config['model_parameters']['random_seed'],
-                                device= config['device'],
-                                root_dir= args.input_directory,
-
-                                # Semi-hyperparameters
-                                input_size = config['data_parameters']['input_size'],
-                                d_input = config['data_parameters']['d_input'], # input_size + 6 (which is exo features)
-                                output_size = config['data_parameters']['output_size'], # must be the same as d_output
-                                d_output = config['data_parameters']['d_output'], # input_size + output_size >= 13
-                                
-                                # Worth tuning hyperparameters
-                                d_model = 128,
-                                q = 4,
-                                v = 4,
-                                h = 4,
-                                N = 1,
-                                attention_size = 4,
-                                dropout = 0,
-                                chunk_mode = None, 
-                                pe = None, 
-                                pe_period = 24,
-                                dataset_name = args.dataset)
+                                d_model = cfg['Transformer_parameter']['d_model'],
+                                q = cfg['Transformer_parameter']['q'],
+                                v = cfg['Transformer_parameter']['v'],
+                                h = cfg['Transformer_parameter']['h'],
+                                N = cfg['Transformer_parameter']['N'],
+                                attention_size = cfg['Transformer_parameter']['attention_size'],
+                                dropout = cfg['Transformer_parameter']['dropout'],
+                                chunk_mode = cfg['Transformer_parameter']['chunk_mode'], 
+                                pe = cfg['Transformer_parameter']['pe'], 
+                                pe_period = cfg['Transformer_parameter']['pe_period']
+                                )
   # Fit model
   # If y_test_df is provided the model # will evaluate predictions on this set every freq_test epochs
   model.fit(X_train_df, y_train_df, X_test_df, y_test_df)
@@ -90,23 +93,20 @@ def main(args):
 
 if __name__ == '__main__':
   # Load cmd line args 
-
+  """Parses the arguments."""
   parser = argparse.ArgumentParser(description='Replicate M4 results for the ESRNN model')
-  parser.add_argument("--dataset", required=True, type=str,
-                      choices=['Yearly', 'Quarterly', 'Monthly', 'Weekly', 'Hourly', 'Daily'],
-                      help="set of M4 time series to be tested")
-  parser.add_argument("--input_directory", required=False, type=str,
-                      help="directory where M4 data will be downloaded")
-  parser.add_argument("--gpu_id", required=False, type=int,
-                      help="an integer that specify which GPU will be used")
-  parser.add_argument("--use_cpu", required=False, type=int,
-                      help="1 to use CPU instead of GPU (uses GPU by default)")
-  parser.add_argument("--num_obs", required=False, type=int,
-                      help="number of M4 time series to be tested (uses all data by default)")
-  parser.add_argument("--test", required=False, type=int,
-                      help="run fast for tests (no test by default)")
-  args = parser.parse_args()
 
-  main(args)
+  parser.add_argument(
+    '--cfg',
+    dest='cfg_file',
+    help='cfg file path',
+    required=True,
+    type=str
+  )
+
+  args = parser.parse_args()
+  cfg.merge_from_file(args.cfg_file)
+  main(cfg)
+  
   
   
